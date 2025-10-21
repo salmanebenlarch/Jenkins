cc153406e9960c7c7d2963783922e4457c320d8e
salmanebenlarch <165717733+salmanebenlarch@users.noreply.github.com>
2025-10-21 11:57:06 +0100

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(25);
+		p.setFirstName("webhook");
+		p.setLastName("testing");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
